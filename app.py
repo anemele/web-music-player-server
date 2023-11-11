02e3eed18123b9dbb3b196a9c96441c5167e2f96
Jason Swift <anemele@outlook.com>
2023-11-11 14:31:21 +0800

    upgrade: random player

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import socket
 from itertools import chain
 from pathlib import Path
 
-from flask import Flask, redirect, render_template, send_file
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -12,23 +12,22 @@ music_list = [
     music.name
     for music in chain.from_iterable(map(root.glob, '*.mp3,*.flac'.split(',')))
 ]
-lens = len(music_list)
 
 
-def rand():
-    return random.randrange(lens)
+def rand_music():
+    return random.choice(music_list)
 
 
 @app.route('/')
 def index():
-    return redirect(f'/page/{rand()}')
-
-
-@app.route('/page/<int:id>')
-def get_page(id):
-    next_id = rand()
-    data = dict(music=music_list[id], next=dict(id=next_id, name=music_list[next_id]))
+    title = rand_music()
+    data = dict(title=title, src=f'/music/{title}')
     return render_template('index.html', **data)
+
+
+@app.route('/random')
+def get_random():
+    return rand_music()
 
 
 @app.route('/music/<music>')
@@ -38,4 +37,4 @@ def get_music(music: str):
 
 if __name__ == '__main__':
     host = socket.gethostbyname(socket.gethostname())
-    app.run(host=host, debug=True)
+    app.run(host=host, debug=False)
