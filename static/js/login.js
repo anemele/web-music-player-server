01ba34f474aba8cbec0e9fa35c25212997db7b33
Jason Swift <anemele@outlook.com>
2023-11-12 20:41:26 +0800

    remove redundancy

--- static/js/login.js
+++ static/js/login.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
-    $('#login').on('click', function () {
-        let pwd = $('#password').val().trim()
-        if (pwd === '') {
-            return
-        }
-        $.ajax({
-            url: '/login',
-            method: 'POST',
-            data: { pwd },
-            success: function (res) {
-                if (res.code == 0) { window.location.pathname = '/' }
-                else { alert('密码错误') }
+
+    $('#password').bind('keydown', function (event) {
+        if (event.keyCode == '13') /* Enter */ {
+
+            let pwd = $(this).val().trim()
+            if (pwd === '') {
+                return
             }
-        })
+
+            $.ajax({
+                url: '/login',
+                method: 'POST',
+                data: { pwd },
+                success: function (res) {
+                    if (res.code == 0) { window.location.pathname = '/' }
+                    else { alert('密码错误') }
+                }
+            })
+        }
     })
 })
