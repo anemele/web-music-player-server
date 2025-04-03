a1d50f9e96989725f1bc5c213e0e360f8bd5cd94
Jason Swift <anemele@outlook.com>
2025-04-03 10:54:36 +0800

    add incremental cache mechanism

--- src/web_music_player/core/core.py
+++ src/web_music_player/core/core.py
@@ -77,33 +77,61 @@ EXPORT_TYPE = tuple[list[MusicItem], list[Path]]
 
 
 def load_data() -> EXPORT_TYPE | None:
-    # 1. 从缓存中加载数据
-    if CACHE_FILE.exists():
-        data = MusicDataCache.from_json(CACHE_FILE.read_bytes())
-        # 2. 判断是否过期
-        if data.expire > time.time():
-            r0 = []
-            r1 = []
-            for i, music in enumerate(data.music):
-                r1.append(music.path)
-                r0.append(MusicItem.from_dict({"id": i, **music.to_dict()}))
-            return r0, r1
+    """
+    加载数据的逻辑：
+    如果缓存文件存在，则读取缓存数据；如果数据未过期，则直接返回；否则更新（机制见下）后返回。
+    如果缓存文件不存在，则重新生成数据并缓存。
 
-    # 3. 缓存不存在或已过期，重新生成数据
+    更新机制：
+    let s1 = 音乐文件路径
+    let s2 = 缓存文件路径
+    保留 s1 与 s2 交集，去除 s2 与 s1 差集，添加 s1 与 s2 差集。
+    然后更新缓存文件。
+    """
+
     if not MUSIC_PATH_FILE.exists():
-        print(f"please set music path in \"{MUSIC_PATH_FILE}\"")
+        print(f'please set music path in "{MUSIC_PATH_FILE}"')
         return None
-
     music_path = MUSIC_PATH_FILE.read_text().strip()
     music_path_list = find_music(Path(music_path))
+
+    def _load_data(data: MusicDataCache) -> EXPORT_TYPE:
+        """将缓存数据转换为导出类型"""
+        r0 = []
+        r1 = []
+        for i, music in enumerate(data.music):
+            r1.append(music.path)
+            r0.append(MusicItem.from_dict({"id": i, **music.to_dict()}))
+        return r0, r1
+
+    if CACHE_FILE.exists():
+        data = MusicDataCache.from_json(CACHE_FILE.read_bytes())
+        # 如果缓存未过期
+        if data.expire > time.time():
+            return _load_data(data)
+
+        s = set(music_path_list)
+        for music in data.music.copy():
+            if music.path in s:
+                s.remove(music.path)
+            else:
+                data.music.remove(music)
+        for path in s:
+            tag = get_tag(Path(path)).to_dict()
+            data.music.append(MusicItemCache(path=Path(path), **tag))
+
+        data.expire = time.time() + EXPIRE_TIME
+        CACHE_FILE.write_bytes(data.to_jsonb())
+        return _load_data(data)
+
     r0 = []
     r1 = []
     cache_data = []
-    for i, music in enumerate(music_path_list):
-        r1.append(music)
-        tag = get_tag(music).to_dict()
+    for i, path in enumerate(music_path_list):
+        tag = get_tag(path).to_dict()
+        r1.append(path)
         r0.append(MusicItem(id=i, **tag))
-        cache_data.append(MusicItemCache(path=music, **tag))
+        cache_data.append(MusicItemCache(path=path, **tag))
 
     # 4. 缓存数据
     expire_time = time.time() + EXPIRE_TIME
